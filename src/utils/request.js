efeeb88f3129f773c63abc545deb30140db95963
leonardo <leonardo.fonseca85300@gmail.com>
2019-12-05 17:10:29 +0100

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,11 +5,7 @@ export default class Req {
     Axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${req.coords.latitude}&lon=${req.coords.longitude}&units=metric&lang=fr&appid=${process.env.REACT_APP_WEATHER_KEY}`
     )
-      .then(function(response) {
-        returnData(response.data);
-      })
-      .catch(function(error) {
-        console.log("error =>", error);
-      });
+      .then(response => returnData(response.data))
+      .catch(error => console.log("error =>", error));
   }
 }
